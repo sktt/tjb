be2fbcf8a13e1dd9014c1819217f3df35b6dccb4
Shakaman0 <sebastian.bertolino@gmail.com>
2013-12-28 22:22:44 +0100

    Scaling factors on maya, character modell, spear

---
+++ Assets/FBXScaleFix.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FBXScaleFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	using UnityEngine;
+	using UnityEditor;
+	
+	class MeshPostprocessor : AssetPostprocessor {
+		
+		void OnPreprocessModel () {
+			(assetImporter as ModelImporter).globalScale = 1000;
+		}
+		
+	}
+}
